b59680f458188114226d69b683a086b6ecd106b7
Michael Marod <mmarod@devis.com>
2015-05-27 16:31:18 -0400

    add meta class

---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+# Includes create::[type]
+#
+# @example Standard configuration
+#   # In your manifest
+#   include '::create'
+#
+#   # In your Hiera data
+#   create::types:
+#     - crons
+#     - execs
+#
+# @param $types [Array] An array of types to include in creation.
+#
+class create (  $types  = [ 'crons',
+                            'execs',
+                            'files',
+                            'packages',
+                            'services',
+                            'ssh_authorized_keys',
+                            'users',
+                            'yumrepos' ] ) {
+    $include_types = regsubst($types,'.*','::create::\0')
+    include $include_types
+}
